1bb17da6eed53642ca9445916644639213d2d03f
Stephan Roslen <stephan@roslen.de>
2022-01-17 18:55:46 +0100

    test skip_newline option

--- tests/skip_whitespace.cpp
+++ tests/skip_whitespace.cpp
@@ -35,3 +35,24 @@ TEST_CASE("skip whitespace", "[skip whitespace]")
     REQUIRE(!result3.has_value());
     REQUIRE(stream.str() == std::string("[1:1] PARSE: Unexpected character:  \n"));
 }
+
+TEST_CASE("skip newline", "[skip whitespace]")
+{
+  auto result1 = test::p1.parse(cstring_buffer("\n0\n1\n2\n"));
+  REQUIRE(result1.has_value());
+  REQUIRE(result1.value() == 42);
+
+  auto result2 = test::p1.parse(cstring_buffer("012"));
+  REQUIRE(result2.has_value());
+  REQUIRE(result2.value() == 42);
+
+  std::stringstream stream3;
+  auto result3 = test::p1.parse(parse_options{}.set_skip_whitespace(false), cstring_buffer("\n0\n12"), stream3);
+  REQUIRE(!result3.has_value());
+  REQUIRE(stream3.str() == std::string("[1:1] PARSE: Unexpected character: \n\n"));
+
+  std::stringstream stream4;
+  auto result4 = test::p1.parse(parse_options{}.set_skip_newline(false), cstring_buffer("\n0\n12"), stream4);
+  REQUIRE(!result4.has_value());
+  REQUIRE(stream4.str() == std::string("[1:1] PARSE: Unexpected character: \n\n"));
+}
